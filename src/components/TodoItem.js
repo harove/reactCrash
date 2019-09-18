475479d6700a5d4ba6ae83970b416ea13f493c18
edgar <harove@gmail.com>
2019-09-18 00:38:12 -0300

    testing binding this to not arrow function

---
+++ src/components/TodoItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Component {
+
+    getStyle = () => {
+       return {
+           background: '#f4f4f4',
+           padding: '10px',
+           borderBotton: '1px #ccc dotted',
+           textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+       }
+    }
+
+    markComplete(e){
+        console.log(this.props)
+    }
+
+
+    render() {
+        return (
+            <div style= { this.getStyle() }>
+                <p> 
+                    <input type="checkbox"  onChange={this.markComplete.bind(this)} /> {' '}
+                    {this.props.todo.title} 
+                
+                </p>
+            </div>
+        )
+    }
+}
+
+
+
+export default TodoItem
+
+
+
